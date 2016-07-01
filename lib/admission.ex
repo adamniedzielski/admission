d7eeafbad40243f09f931f5fe7e0fcf179db6a20
Adam Niedzielski <adamsunday@gmail.com>
2016-07-01 11:46:30 +0200

    Raise a meaningful exception when provided directory cannot be found.

--- lib/admission.ex
+++ lib/admission.ex
@@ -4,12 +4,19 @@ defmodule Admission do
 
   def main(args) do
     [directory] = args
+    ensure_directory_exists(directory)
     config = load_config(directory)
     Builder.generate_pdf(config, directory)
     Builder.generate_epub(config, directory)
     Builder.generate_mobi(directory)
     Builder.generate_html_previews(config, directory)
     Submitter.send_files_to_api(config, directory)
+  end
+
+  defp ensure_directory_exists(directory) do
+    if !File.exists?(directory) do
+      raise ArgumentError, message: "Provided directory does not exists"
+    end
   end
 
   defp load_config(directory) do
